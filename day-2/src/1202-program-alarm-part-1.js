c2a9e5c0a73184fd366c1811a657723d40448414
Jamie Preston <jamie.preston@clicktravel.com>
2019-12-16 14:00:13 +0000

    Create Intcode Computer to solve day 2 part 1

--- day-2/src/1202-program-alarm-part-1.js
+++ day-2/src/1202-program-alarm-part-1.js
@@ -1,8 +1,61 @@
 // https://adventofcode.com/2019/day/2
+const calculateValue = (code, position) => {
+    const [
+      command,
+      firstPos,
+      secondPos,
+      indexToUpdate,
+    ] = code.slice(position, position + 4);
+
+    const firstVal = code[firstPos];
+    const secondVal = code[secondPos];
+
+    const newValue = command === 1
+      ? firstVal + secondVal
+      : firstVal * secondVal;
+
+    return {
+      newValue,
+      indexToUpdate,
+    };
+};
 
 class IntcodeComputer {
+
   constructor(code) {
     this.code = code;
+    this.currentPosition = 0;
+  }
+
+  singleStep() {
+    const command = this.code[this.currentPosition];
+
+    if (![1, 2, 99].includes(command)) {
+      throw new Error(`Unrecognized command: ${command}`);
+    }
+
+    if (command === 99) {
+      this.currentPosition = this.currentPosition + 1;
+      return 'STOP';
+    }
+
+    const {
+      newValue,
+      indexToUpdate,
+    } = calculateValue(this.code, this.currentPosition);
+    
+    this.code[indexToUpdate] = newValue;
+    this.currentPosition = this.currentPosition + 4;
+
+    return 'CONTINUE';
+  };
+
+  execute() {
+    let action = 'START';
+    while (action !== 'STOP') {
+      action = this.singleStep();
+    }
+    return this.code;
   }
 };
 
--- day-2/test/1202-program-alarm-part-1.spec.js
+++ day-2/test/1202-program-alarm-part-1.spec.js
@@ -4,8 +4,108 @@ import code from './test-data.js';
 describe('1202-program-alarm-part-1', () => {
   describe('#opcodeComputer', () => {
     it('should initialise', () => {
+      const inputCode = [
+        1,9,10,3,
+        2,3,11,0,
+        99,
+        30,40,50
+      ];
+      const intcodeComputer = new IntcodeComputer(inputCode);
+      expect(intcodeComputer.code).toEqual(inputCode);
+    });
+
+    describe('singleStep', () => {
+      it('should step through once', () => {
+        const inputCode = [
+          1,9,10,3,
+          2,3,11,0,
+          99,
+          30,40,50
+        ];
+        const intcodeComputer = new IntcodeComputer(inputCode);
+        intcodeComputer.singleStep();
+        const expectedResult = [
+          1,9,10,70,
+          2,3,11,0,
+          99,
+          30,40,50
+        ];
+        expect(intcodeComputer.code).toEqual(expectedResult);
+      });
+
+      it('should step through twice', () => {
+        const inputCode = [
+          1,9,10,3,
+          2,3,11,0,
+          99,
+          30,40,50
+        ];
+        const intcodeComputer = new IntcodeComputer(inputCode);
+        intcodeComputer.singleStep();
+        intcodeComputer.singleStep();
+        const expectedResult = [
+          3500,9,10,70,
+          2,3,11,0,
+          99,
+          30,40,50
+        ];
+        expect(intcodeComputer.code).toEqual(expectedResult);
+      });
+    });
+  });
+
+  describe('execute', () => {
+    it('should execute a single step program with addition', () => {
+      const inputCode = [1,0,0,0,99];
+      const intcodeComputer = new IntcodeComputer(inputCode);
+      const expectedResult = [2,0,0,0,99];
+      const result = intcodeComputer.execute();
+
+      expect(result).toEqual(expectedResult);
+    });
+    it('should execute a single step program with multiplication', () => {
+      const inputCode = [2,3,0,3,99];
+      const intcodeComputer = new IntcodeComputer(inputCode);
+      const expectedResult = [2,3,0,6,99];
+      const result = intcodeComputer.execute();
+
+      expect(result).toEqual(expectedResult);
+    });
+
+    it('should execute a multi step program', () => {
+      const inputCode = [1,1,1,4,99,5,6,0,99];
+      const intcodeComputer = new IntcodeComputer(inputCode);
+      const expectedResult = [30,1,1,4,2,5,6,0,99];
+      const result = intcodeComputer.execute();
+
+      expect(result).toEqual(expectedResult);
+    });
+
+    it('should execute a multi step program and stop at 99', () => {
+      const inputCode = [
+        1,9,10,3,
+        2,3,11,0,
+        99,
+        30,40,50
+      ];
+      const intcodeComputer = new IntcodeComputer(inputCode);
+      const expectedResult = [
+        3500,9,10,70,
+        2,3,11,0,
+        99,
+        30,40,50
+      ];
+      const result = intcodeComputer.execute();
+
+      expect(result).toEqual(expectedResult);
+    });
+
+    it('should solve the given puzzle', () => {
       const intcodeComputer = new IntcodeComputer(code);
-      expect(intcodeComputer.code).toEqual(code);
+      const expectedResult = 4090701;
+      const [ result ] = intcodeComputer.execute();
+
+      expect(result).toEqual(expectedResult);
     });
   });
 });
