96fc40fb0dd0a8041d904ea5e38880216909bcdf
jamiepreston <jamie.preston@gmail.com>
2021-01-20 08:35:37 +0000

    Update 1202-program-alarm-part-1.spec.js

--- day-2/test/1202-program-alarm-part-1.spec.js
+++ day-2/test/1202-program-alarm-part-1.spec.js
@@ -2,6 +2,7 @@ import IntcodeComputer from '../src/1202-program-alarm-part-1';
 import code from './test-data.js';
 
 describe('1202-program-alarm-part-1', () => {
+  
   describe('#opcodeComputer', () => {
     it('should initialise', () => {
       const inputCode = [
